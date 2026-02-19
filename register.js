186c4ea2e5a68e9f2e8d3253def3bcbaa5a016b7
desh-aditya-ch <deshaditya7@gmail.com>
2026-02-20 02:52:25 +0530

    registering

---
+++ register.js
@@ -0,0 +1,46 @@
+const express=require("express");
+
+const app=express();
+
+const PORT=3000;
+
+app.use(express.json());
+
+
+const validateJsonHeader=(req,res,next)=>{
+    const contentType= req.headers['content-type'];
+
+    if(contentType!=='application/json'){
+        return res.status(400).json({
+            error:"content-type is Incorrect"
+        })
+    }
+    next();
+}
+
+
+
+app.post("/register",validateJsonHeader,(req,res)=>{
+    const {username,password}=req.body;
+
+    if(!username || !password){
+        return res.status(400).json({
+            error:"username and password is required"
+        })
+    }
+
+    console.log(`user is registered by username : ${username}`);
+
+    res.status(200).json({
+        message:"user registered successfully",
+        username: username
+    })
+})
+
+
+app.listen(
+    PORT,
+    ()=>{console.log(`Server running at http://localhost:${PORT}`);
+    }
+
+)
